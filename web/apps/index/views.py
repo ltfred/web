5a8ddb1883d64226fb22f5d6f76e327f7e4b3637
ltfred <ltfred@163.com>
2020-03-09 18:15:38 +0800

    文章文章

--- web/apps/index/views.py
+++ web/apps/index/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from article.models import ArticleDetail
+from article.models import ArticleDetail, ArticleCategory
 from index.models import Carousel
 
 
@@ -10,8 +10,17 @@ class IndexView(View):
 
         new_articles = ArticleDetail.objects.all()[0:8]
         carousel_article = Carousel.objects.filter(is_active=True)
+        parent_category = ArticleCategory.objects.filter(parent__isnull=True)
+        categories = []
+        for category in parent_category:
+            sub_categories = ArticleCategory.objects.filter(parent=category)
+            categories.append({
+                'category': category,
+                'sub_categories': sub_categories
+            })
         data = {
             'new_articles': new_articles,
-            'carousel_article': carousel_article
+            'carousel_article': carousel_article,
+            'categories': categories
         }
         return render(request, 'index.html', context=data)
