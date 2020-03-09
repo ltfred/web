9ee036bcbef453a15f3ea5052f13bb2cb48455b0
ltfred <ltfred@163.com>
2020-03-09 10:48:11 +0800

    详情页跳转

--- web/apps/article/urls.py
+++ web/apps/article/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import ArticleDetailView
 
 urlpatterns = [
-
+    path("detail/<int:article_id>/", ArticleDetailView.as_view(), name='detail')
 ]
--- web/apps/article/views.py
+++ web/apps/article/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
 
-# Create your views here.
+from article.models import ArticleDetail
+
+
+class ArticleDetailView(View):
+
+    def get(self, request, article_id):
+
+        try:
+            article = ArticleDetail.objects.get(id=article_id)
+        except:
+            raise
+
+        context = {"article": article}
+
+        return render(request, 'article_detail.html', context)
