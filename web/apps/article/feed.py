8040e99803bf8f2b2150319994b93ebf9c33b84f
ltfred <ltfred@163.com>
2020-06-27 14:36:31 +0800

    rss订阅

---
+++ web/apps/article/feed.py
@@ -0,0 +1,28 @@
+from django.contrib.syndication.views import Feed
+from django.shortcuts import reverse
+from .models import ArticleDetail
+
+
+class BlogFeed(Feed):
+    # 标题
+    title = 'jiangxfone的个人博客'
+    # 描述
+    description = '一个用来练习Django的个人博客'
+    # 链接
+    link = "/"
+
+    def items(self):
+        # 返回所有文章
+        return ArticleDetail.objects.all().order_by("-create_time")
+
+    def item_title(self, item):
+        # 返回文章标题
+        return item.title
+
+    def item_description(self, item):
+        # 返回文章描述
+        return item.des
+
+    def item_link(self, item):
+        # 返回文章详情页的路由
+        return reverse('article:detail', args=(item.id,))
--- web/apps/article/urls.py
+++ web/apps/article/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+
+from . import feed
 from .views import ArticleDetailView, ArticleCategoryView, ArticleStarView, LabelArticleView, MySearchView, TimeLineView
 
 app_name = 'article'
@@ -11,4 +13,6 @@ urlpatterns = [
     # path('categories/', GetCategories.as_view()),
     path('search', MySearchView(), name='search'),
     path('time', TimeLineView.as_view(), name='time'),
+    path('rss/', feed.BlogFeed(), name='rss'),
+
 ]
