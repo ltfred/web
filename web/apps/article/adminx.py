6701378176d137c10c7178d936e055011697afdf
ltfred <ltfred@163.com>
2020-06-27 17:33:26 +0800

    优化首页限时效果

--- web/apps/article/adminx.py
+++ web/apps/article/adminx.py
@@ -16,9 +16,15 @@ class ArticleXadmin(object):
 class ArticleCategoryXadmin(object):
     list_display = ['id', 'name', 'parent']
 
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'parent':
+            kwargs['queryset'] = ArticleCategory.objects.filter(parent__isnull=True)
+        return super(ArticleCategoryXadmin, self).formfield_for_dbfield(db_field, **kwargs)
+
 
 class ArticleLabelXadmin(object):
     list_display = ['id', 'name']
+
 
 xadmin.site.register(ArticleDetail, ArticleXadmin)
 xadmin.site.register(ArticleCategory, ArticleCategoryXadmin)
--- web/apps/index/views.py
+++ web/apps/index/views.py
@@ -12,11 +12,11 @@ class IndexView(TemplateView):
         content["new_articles"] = ArticleDetail.objects.all()[0:12]
         content["carousel_article"] = Carousel.objects.filter(is_active=True)
         content["categories"] = ArticleCategory.get_categories()
-        content["tech_articles"] = ArticleDetail.objects.filter(category1_id=5).order_by('-view_count')[0:5]
-        content["life_articles"] = ArticleDetail.objects.filter(category1_id=2).order_by('-view_count')[0:5]
-        content["tech_new_articles"] = ArticleDetail.objects.filter(category1_id=5).order_by('-create_time')[0:8]
-        content["shi_count"] = ArticleDetail.objects.filter(category1_id=1).count()
-        content["tech_count"] = ArticleDetail.objects.filter(category1_id=2).count()
-        content["tool_count"] = ArticleDetail.objects.filter(category1_id=3).count()
-        content["ci_count"] = ArticleDetail.objects.filter(category1_id=6).count()
+        content["tech_articles"] = ArticleDetail.objects.filter(category1_id=4).order_by('-view_count')[0:5]
+        content["life_articles"] = ArticleDetail.objects.filter(category2_id=7).order_by('-view_count')[0:5]
+        content["tech_new_articles"] = ArticleDetail.objects.filter(category1_id=4).order_by('-create_time')[0:8]
+        content["shi_count"] = ArticleDetail.objects.filter(category1_id=2).count()
+        content["tech_count"] = ArticleDetail.objects.filter(category1_id=4).count()
+        content["tool_count"] = ArticleDetail.objects.filter(category1_id=5).count()
+        content["ci_count"] = ArticleDetail.objects.filter(category2_id=8).count()
         return content
