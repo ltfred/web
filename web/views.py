12cc8055d587b23374ec9697b6d9db3c4825f639
ltfred <ltfred@163.com>
2020-08-27 19:59:57 +0800

    style

--- web/views.py
+++ web/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
 
-
 def page_not_found(request, exception, template_name='error_page.html'):
-    return render(request, template_name, {"msg": 404, "msg": "Sorry! 页面找不到了"})
+    return render(request, template_name, {"code": 404, "msg": "Sorry! 页面找不到了"})
+
 
 def server_error(request, exception, template_name='error_page.html'):
     return render(request, template_name, {"code": 500, "msg": "Sorry! 服务器出了点问题"})
